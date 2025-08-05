f410506000756b882ff9fcffa899a76ff3abb4a5
Oscar Serna <oscar.eduardo.serna@gmail.com>
2025-08-05 14:34:50 -0500

    fixed tests

--- test/validation/policy/policies.t.sol
+++ test/validation/policy/policies.t.sol
@@ -778,7 +778,7 @@ abstract contract policies is RulesEngineCommon {
         assertEq(data.policyDescription, policyDescription);
     }
 
-    function testRulesEngine_Unit_deletePolicy() public {
+    function testRulesEngine_Unit_deletePolicy() public ifDeploymentTestsEnabled endWithStopPrank {
         vm.startPrank(user1);
         uint policyId = _createBlankPolicy();
 
@@ -876,7 +876,7 @@ abstract contract policies is RulesEngineCommon {
             rule.negEffects = new Effect[](1);
             rule.negEffects[0] = effectId_revert;
             // we check that a rule cannot be made for the erased policy
-            vm.expectRevert("PolicyId is invalid");
+            vm.expectRevert("Policy does not exist");
             ruleId = RulesEngineRuleFacet(address(red)).updateRule(1, 0, rule, "My rule", "My way or the highway");
         }
         // we check that we can't create a calling function for a deleted policy
