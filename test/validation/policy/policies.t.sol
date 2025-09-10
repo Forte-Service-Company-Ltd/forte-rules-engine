723f95b48933799cda8c3b0e8668caf9a663e661
Shane Duncan <106779100+ShaneDuncan602@users.noreply.github.com>
2025-09-10 13:00:31 -0500

    Removed the string.sol import so it uses foundry's

--- test/validation/policy/policies.t.sol
+++ test/validation/policy/policies.t.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.24;
 
 import "test/utils/RulesEngineCommon.t.sol";
-import {Strings} from "@openzeppelin/utils/Strings.sol";
 
 abstract contract policies is RulesEngineCommon {
     /**
