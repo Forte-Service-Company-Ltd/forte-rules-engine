2cd502cc406ca768942148c09e3c29e1741ced5b
Oscar Serna <oscar.eduardo.serna@gmail.com>
2025-09-11 21:45:13 -0500

    added mapping for pfcAdmin to policies

--- src/engine/RulesEngineStorageStructure.sol
+++ src/engine/RulesEngineStorageStructure.sol
@@ -145,6 +145,7 @@ struct ForeignCallStorage {
     mapping(address foreignCallAddress => mapping(bytes4 signature => bool)) isPermissionedForeignCall; // Store all permissioned foreign calls within the rules engine
     mapping(address foreignCallContractAddress => mapping(bytes4 => mapping(address permissionedAdmin => bool))) permissionedForeignCallAdmins; // This is used to store the addresses of all permissioned foreign call admins for look ups
     mapping(address foreignCallContractAddress => mapping(bytes4 selector => address[])) permissionedForeignCallAdminsList; // This is used to store the addresses of all permissioned foreign call admins for look ups
+    mapping(address admin => mapping(address foreignCallContractAddress => mapping(bytes4 selector => uint[] policyIds))) permissionedFCAdminToPoliciesUsingFC; // This is used to store the policy Ids that are using a permissioned foreign call for a given admin
 }
 
 /**
